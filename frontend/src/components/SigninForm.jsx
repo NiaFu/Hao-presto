6c5ce287ebea90e8598fcf40f3e89569897fd789
z5511846 <z5511846@ad.unsw.edu.au>
2024-11-13 23:16:38 +1100

    test

--- frontend/src/components/SigninForm.jsx
+++ frontend/src/components/SigninForm.jsx
@@ -1,2 +1,11 @@
 import React, {useState, useEffect} from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+
+const SigninForm=() => {
+
+    return(
+        <div>
+            Hello!
+        </div>
+    )
+}
