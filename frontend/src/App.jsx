b32cb5e6a3db9cc5ccc3993df6ac6988a4fedb3a
z5511846 <z5511846@ad.unsw.edu.au>
2024-11-14 05:24:30 +1100

    add-component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SigninForm from './components/SigninForm';
 import SignupForm from './components/SignupForm';
 import LogOut from './components/LogOut';
+import DashBoard from './components/DashBoard';
 
 const welcomeStyle = {
   display: 'flex',
@@ -30,6 +31,7 @@ function App() {
     <Route path="/" element={<div style={welcomeStyle}>Welcome Back PRESTO!</div>} />
       <Route path="/login" element={<SigninForm setToken = {setToken} />}></Route> 
       <Route path="/register" element={<SignupForm setToken = {setToken}/>}></Route>
+      <Route path="/dashboard" element={<DashBoard/>}></Route>
     </Routes>
 
     </BrowserRouter>
