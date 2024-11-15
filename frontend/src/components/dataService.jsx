045dc753eebcd26eadd0a713501897234f32ba81
z5511846 <z5511846@ad.unsw.edu.au>
2024-11-15 12:43:30 +1100

    lint-style

--- frontend/src/components/dataService.jsx
+++ frontend/src/components/dataService.jsx
@@ -1,42 +1,41 @@
 // Get data
 export const getStore = () => {
-    const usertoken = localStorage.getItem('token');
-  
-    return fetch('http://localhost:5005/store', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${usertoken}`,
-        'Content-Type': 'application/json',
-      },
+  const usertoken = localStorage.getItem('token');
+
+  return fetch('http://localhost:5005/store', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${usertoken}`,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    });
+};
+
+// Update the data
+export const updateStore = (updatedStore) => {
+  return fetch('http://localhost:5005/store', {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ store: updatedStore }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      console.log('Updated store');
+      return response.json();
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      });
-  };
-  
-  // Update the data
-  export const updateStore = (updatedStore) => {
-    return fetch('http://localhost:5005/store', {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ store: updatedStore }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        console.log('Updated store');
-        return response.json();
-      })
-      .catch((error) => {
-        console.error('Error updating store data:', error);
-        throw error;
-      });
-  };
-  
+    .catch((error) => {
+      console.error('Error updating store data:', error);
+      throw error;
+    });
+};
