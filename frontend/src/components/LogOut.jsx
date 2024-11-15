f876b573bbba4ebe214688b91685a1c386403c9f
z5511846 <z5511846@ad.unsw.edu.au>
2024-11-15 11:35:40 +1100

    lint-Logout

--- frontend/src/components/LogOut.jsx
+++ frontend/src/components/LogOut.jsx
@@ -1,35 +1,33 @@
-import React, {useState, useEffect} from "react";
-import { BrowserRouter, Routes, Route, useNavigate, Link } from 'react-router-dom'; 
+import { useNavigate, Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
 
-const LogOut = (props) =>{
-    const navigate = useNavigate();
+const LogOut = (props) => {
+  const navigate = useNavigate();
 
-    const out = () =>{
-        props.setToken(null);
-        localStorage.removeItem('token');
-        navigate('/')
-    }
+  const out = () => {
+    props.setToken(null);
+    localStorage.removeItem('token');
+    navigate('/');
+  };
 
-
-    return(
+  return (
+    <>
+      {props.token === null ? (
         <>
-        {props.token === null 
-                ? (
-                    <>
-                        <Button component = {Link} to = "/login" variant="contained">LOGIN</Button>
-                        <Button component = {Link} to = "/register" variant="contained">REGISTER</Button>
-                    </>
-                ) : (
-                    <>
-                        <Button variant="outlined" onClick={out}>Log Out</Button>
-                    </>
-                )
-            }
+          <Button component={Link} to="/login" variant="contained">
+            LOGIN
+          </Button>
+          <Button component={Link} to="/register" variant="contained">
+            REGISTER
+          </Button>
         </>
-    )
-}
+      ) : (
+        <Button variant="outlined" onClick={out}>
+          Log Out
+        </Button>
+      )}
+    </>
+  );
+};
 
 export default LogOut;
